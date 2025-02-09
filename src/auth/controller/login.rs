174dbb4fd7e39c63f5ad9ce247fcd246b9592441
Angelillo15 <contact@angelillo15.es>
2025-02-09 12:13:54 +0100

    controller(login): change web::Json -> ValidatedJson

--- src/auth/controller/login.rs
+++ src/auth/controller/login.rs
@@ -1,19 +1,17 @@
 use std::sync::Arc;
-use actix_web::{get, post, web, HttpResponse, Responder, Scope};
-use crate::auth::service::auth::{AuthService, LoginRequest};
+use actix_web::{post, web, HttpResponse, Responder, Scope};
+use crate::actix::validated_json::ValidatedJson;
+use crate::auth::request::login::LoginRequest;
+use crate::auth::service::auth::{AuthService};
 
 #[post("/login")]
-async fn login(login_service: web::Data<Arc<dyn AuthService>>, request: web::Json<LoginRequest>) -> impl Responder {
-    println!("{:?}", request);
-    request.username.clone();
-    HttpResponse::Ok()
-}
-
-#[get("/")]
-async fn index() -> impl Responder {
-    "Buenos dias"
+async fn login(login_service: web::Data<Arc<dyn AuthService>>, request: ValidatedJson<LoginRequest>) -> impl Responder {
+    match login_service.login(request.clone()).await {
+        Ok(response) => HttpResponse::Ok().json(response),
+        Err(error) => HttpResponse::BadRequest().json(error),
+    }
 }
 
 pub fn endpoints(scope: Scope) -> Scope {
-    scope.service(login).service(index)
+    scope.service(login)
 }
