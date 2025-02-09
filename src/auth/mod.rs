7b3eaf51d4a694a2b237eba9c7de847bf15acd04
Angelillo15 <contact@angelillo15.es>
2025-02-09 12:10:18 +0100

    lib: add module definitions

--- src/auth/mod.rs
+++ src/auth/mod.rs
@@ -1,5 +1,5 @@
-pub mod service;
+pub mod controller;
 pub mod error;
 pub mod infrastructure;
-
-pub mod controller;
+pub mod service;
+pub mod request;
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,5 @@
+pub mod actix;
 pub mod auth;
+pub mod config;
 pub mod entity;
+pub mod util;
