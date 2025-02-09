e34a18e5ec743c328e66de9e75720088fdfa52c5
Angelillo15 <contact@angelillo15.es>
2025-02-09 14:41:54 +0100

    feat(cli): initial clap support

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,39 @@
-use std::env::set_var;
-use std::sync::Arc;
-use actix_web::{web, App, HttpServer};
 use actix_web::web::Data;
+use actix_web::{web, App, HttpServer};
+use clap::Parser;
+use directories::ProjectDirs;
 use env_logger::Env;
-use sea_orm::{Database, DatabaseConnection};
 use panel::auth;
 use panel::auth::infrastructure::sql::auth_impl::SQLImpl;
 use panel::auth::service::auth::AuthService;
+use sea_orm::{Database, DatabaseConnection};
+use std::env::set_var;
+use std::sync::Arc;
+use log::debug;
+
+#[derive(Parser, Debug, Clone)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long, default_value_t = default_config_path())]
+    config_path: String,
+    #[arg(long)]
+    host: Option<String>,
+    #[arg(short, long)]
+    port: Option<u16>,
+    #[arg(long, short, action)]
+    debug: bool,
+}
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    set_var("RUST_LOG", "debug");
+    let args = Args::parse();
+
+    if args.debug {
+        set_var("RUST_LOG", "debug");
+    }
+
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    debug!("Debug mode has been enabled!");
 
     let db: DatabaseConnection = Database::connect("mysql://root@localhost/panel")
         .await
@@ -29,4 +51,11 @@ async fn main() -> std::io::Result<()> {
     .bind(("127.0.0.1", 8080))?
     .run()
     .await
+}
+
+fn default_config_path() -> String {
+    match ProjectDirs::from("es", "angelillo15", "panel") {
+        Some(dir) => format!("{}/config.toml", dir.config_dir().to_str().unwrap()).to_string(),
+        None => "./config.toml".to_string(),
+    }
 }
