17f747315e945fa5e05e1a3a47dc29cf9bbff672
Angelillo15 <contact@angelillo15.es>
2025-02-09 16:04:40 +0100

    feat: pass HashService to AuthService SQLImpl

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,14 @@ use actix_web::{web, App, HttpServer};
 use clap::Parser;
 use directories::ProjectDirs;
 use env_logger::Env;
+use log::debug;
 use panel::auth;
 use panel::auth::infrastructure::sql::auth_impl::SQLImpl;
 use panel::auth::service::auth::AuthService;
+use panel::auth::service::hash_service::HashService;
 use sea_orm::{Database, DatabaseConnection};
 use std::env::set_var;
 use std::sync::Arc;
-use log::debug;
 
 #[derive(Parser, Debug, Clone)]
 #[command(version, about, long_about = None)]
@@ -39,7 +40,11 @@ async fn main() -> std::io::Result<()> {
         .await
         .expect("database connection");
 
-    let auth_service: Data<Arc<dyn AuthService>> = Data::new(Arc::new(SQLImpl::new(db.clone())));
+    // TODO: change this in future
+    let hash_service = Arc::new(HashService::new("todotochangethisinthefuture"));
+
+    let auth_service: Data<Arc<dyn AuthService>> =
+        Data::new(Arc::new(SQLImpl::new(db.clone(), hash_service.clone())));
 
     HttpServer::new(move || {
         let mut app = App::new();
